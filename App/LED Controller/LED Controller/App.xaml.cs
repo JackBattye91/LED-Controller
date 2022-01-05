5a64d7a0f23823a6dbf2a82b2db2128bb8ded780
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-01-05 14:34:47 +0000

    App files Added

--- App/LED Controller/LED Controller/App.xaml.cs
+++ App/LED Controller/LED Controller/App.xaml.cs
@@ -10,7 +10,7 @@ namespace LED_Controller
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
