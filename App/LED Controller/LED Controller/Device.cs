fd96a7853fc799d29a5e2935757073d73e56e432
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2022-01-05 17:17:50 +0000

    Added device classes
    - Added Device class
    - Added Led Device class

---
+++ App/LED Controller/LED Controller/Device.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LED_Controller
+{
+    public abstract class Device
+    {
+        public Guid UUID { get; private set; }
+        public string Name { get; set; }
+        public List<string> Features { get; set; }
+
+        public Device()
+        {
+            UUID = Guid.NewGuid();
+            Features = new List<string>();
+        }
+        public Device(Guid uuid)
+        {
+            UUID = uuid;
+            Features = new List<string>();
+        }
+
+        public override string ToString()
+        {
+            return "{ " + $"'UUID' : {UUID}, 'Name' : {Name}" + "}"; 
+        }
+    }
+}
---
+++ App/LED Controller/LED Controller/DevicePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LED_Controller
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DevicePage : ContentPage
+    {
+        public DevicePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
